ded0ecc99f57cf1091566dd02904efd7c1849eab
Jiri <javora@cngroup.dk>
2021-08-10 15:42:14 +0200

    Added V2 of CW5

---
+++ CW5_ReduceV2.js
@@ -0,0 +1,8 @@
+function grow(x){ //using forEach and multiplying and returning a separate variable is also possible
+    const arrayIterator = x[Symbol.iterator]();
+    let grownArray = 1;
+    for (const element of arrayIterator) {
+      grownArray *= element
+    }
+    return grownArray
+  }
