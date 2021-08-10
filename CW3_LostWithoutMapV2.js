c970b8d65b4a3c63437777223bdd618d90997d76
Jiri <javora@cngroup.dk>
2021-08-10 14:03:45 +0200

    CW3 V2 - using forEach

---
+++ CW3_LostWithoutMapV2.js
@@ -0,0 +1,7 @@
+function maps(x){ //basically the same thing can be done with a for loop, but since I've already written one it seems pointless
+    const doubledArray = [];
+    x.forEach(element => {
+      doubledArray.push(element * 2)
+    });
+    return doubledArray
+  }
