f364f9fef627dddfa5a226e2a461605828019237
Jiri <javora@cngroup.dk>
2021-08-10 11:29:01 +0200

    CW2 1st iteration

---
+++ CW2_HumanCatDogYears.js
@@ -0,0 +1,5 @@
+var humanYearsCatYearsDogYears = function(humanYears) {
+    // Your code here!
+    return [humanYears,15+(humanYears > 1 ? (humanYears * 4) + 1 : 0 ),15+(humanYears > 1 ? (humanYears * 5) - 1 : 0 )];
+  }
+  
