e7b53526a38070c4cfd2efce532b33544ed06bc7
Jiri <javora@cngroup.dk>
2021-08-13 10:06:01 +0200

    Second version of CW6 - initials - improved on standup

--- CW6_Initials.js
+++ CW6_Initials.js
@@ -1,5 +1,19 @@
-function abbrevName(name){
-    const array = [...name.matchAll(/(?<=\b)[A-Z]/gi)];
-    
-    return `${array[0].toString().toUpperCase()}.${array[1].toString().toUpperCase()}`;
-  }
+function abbrevName(name) {
+  const array = [...name.matchAll(/(?<=\b)[A-Z]/gi)];
+
+  return `${array[0].toString().toUpperCase()}.${array[1]
+    .toString()
+    .toUpperCase()}`;
+}
+
+const firstLetterUpperCase = (string) => string[0].toUpperCase();
+
+function abbrevName2(name) {
+  const splitName = name.split(" ");
+
+  return (
+    firstLetterUpperCase(splitName[0]) +
+    "." +
+    firstLetterUpperCase(splitName[1])
+  );
+}
